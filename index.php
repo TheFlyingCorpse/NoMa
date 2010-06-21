871399b4010ac8e298e3ae597c2e265b3c4bf7f5
wpreston <wpreston@ws-wpreston.int.netways.de>
2010-06-21 19:27:08 +0200

    * added google tracking ID

--- index.php
+++ index.php
@@ -278,11 +278,11 @@ if (file_exists('doc/VERSION.txt') && is_readable('doc/VERSION.txt')) {
 	}
 }
 $html->assign('VERSION', $version);
+if (!empty($_SERVER['TRACKING_ID'])) {
+    $html->assign('TRACKING_ID', '<script type="text/javascript">var gaJsHost=(("https:"==document.location.protocol)?"https://ssl.":"http://www.");document.write(unescape("%3Cscript src=\'"+gaJsHost+"google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));</script><script type="text/javascript">try{var pageTracker=_gat._getTracker("'.$_SERVER['TRACKING_ID'].'");pageTracker._trackPageview();}catch(err){}</script>');
+}
 
 $html->assign('MAIN_CONTENT', getContent());
 $html->show();
-if (defined($_SERVER['TRACKING_ID'])) {
-    $html->assign('TRACKING_ID', '<script type="text/javascript">var gaJsHost=(("https:"==document.location.protocol)?"https://ssl.":"http://www.");document.write(unescape("%3Cscript src=\'"+gaJsHost+"google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));</script><script type="text/javascript">try{var pageTracker=_gat._getTracker("'.$_SERVER['TRACKING_ID'].'");pageTracker._trackPageview();}catch(err){}</script>');
-}
 
 ?>
