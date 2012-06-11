TheFlyingCorpse/NoMa

commit 988aa5d36de7dd679ce2233b7c63b54bef19803e

fixes #1394 fixes #1444
Rollover bug resolved
Filtering on state transitions

notifier/noma_daemon.pl.in
@@ -130,6 +130,14 @@ my %stati_service = (
     'CRITICAL' => 'on_critical',
     'UNKNOWN'  => 'on_unknown'
 );
+
+# Used for state transitins
+my %stati_previous = (
+    'OK'       => 2,
+    'WARNING'  => 4,
+    'CRITICAL' => 8,
+    'UNKNOWN'  => 16
+);
 my %stati_host = (
     'UP'          => 'on_host_up',
     'UNREACHABLE' => 'on_host_unreachable',
@@ -382,6 +390,9 @@ do
 
 
 
+            # retrieve the previous check result to allow for transition notifications
+            # e.g. WARNING -> CRITICAL
+            my $last_state = getLastState($cmdh{host}, $cmdh{service});
 
             # generate query and get list of possible users to notify
             my $query =
@@ -389,9 +400,13 @@ do
             if ( $cmdh{check_type} eq 'h' )
             {
                 $query .= ' where ' . $stati_host{$cmdh{status}} . '=\'1\' and ' . $stati_type{$cmdh{notification_type}} . '=\'1\'';
+            } elsif ( $cmdh{notification_type} eq 'PROBLEM' || $cmdh{notification_type} eq 'RECOVERY')
+            {
+                $query .= ' where (' . $stati_service{$cmdh{status}} . ' & 1) = 1';
+                $query .= ' OR (' . $stati_service{$cmdh{status}} . ' & ' .$stati_previous{$last_state} .') = ' .$stati_previous{$last_state};
             } else
             {
-                $query .= ' where ' . $stati_service{$cmdh{status}} . '=\'1\' and ' . $stati_type{$cmdh{notification_type}} . '=\'1\'';
+                $query .= ' where (' . $stati_service{$cmdh{status}} . ' & 1) = 1 and ' . $stati_type{$cmdh{notification_type}} . '=\'1\'';
             }
 
             # only active rules!
@@ -508,7 +523,7 @@ do
                         if (counterExceededMax(\@id_arr, $notificationCounter))
                         {
                             debug('No more alerts possible, rolling over the counter', 2);
-                            $notificationCounter = resetNotificationCounter($cmdh{host}, $cmdh{service});
+                            $notificationCounter = 1;
                         }
 
                         # get contact data
@@ -945,6 +960,37 @@ sub getNotificationCounter
 
 
 }
+
+# returns UP or OK if the state is not in the table already, otherwise returns the last known state
+sub getLastState
+{
+    my ($host, $svc) = @_;
+    my $check_result;
+
+
+    my $query = 'select check_result from notification_stati where host=\''.$host.'\'';
+
+    if (defined($svc) and $svc ne '')
+    {
+        # service alert
+        $query .= ' and service=\''.$svc.'\'';
+        $check_result = "OK";
+    }
+    else
+    {
+        $query .= ' and check_type=\'h\'';
+        $check_result = "UP";
+    }
+
+    my %dbResult = queryDB($query);
+
+    $check_result = $dbResult{0}->{check_result}
+      if ( defined( $dbResult{0}->{check_result} ) );
+
+    return $check_result;
+
+
+}
 sub prepareNotification
 {
 	my ($incident_id, $user, $method, $short_cmd, $dest, $sender, $id,
@@ -1353,9 +1399,9 @@ sub counterExceededMax
 {
     my ($ids, $counter) = @_;
 
-    my $query = 'select notify_after_tries from notifications where id in ('.join(',',@$ids).') and rollover=1';
+    my $query = 'select notify_after_tries from notifications where id in ('.join(',',@$ids).') and rollover=1 and active=1';
     $query .= ' union select e.notify_after_tries from escalations_contacts as e';
-    $query .= ' left join notifications as n on e.notification_id=n.id where notification_id in ('.join(',',@$ids).') and rollover=1';
+    $query .= ' left join notifications as n on e.notification_id=n.id where notification_id in ('.join(',',@$ids).') and rollover=1 and active=1';
 
 	my @dbResult = queryDB($query, '1');
 

configure.ac
@@ -1,6 +1,6 @@
 AC_PREREQ([2.61])
 AC_INIT(noma,1)
-VERSION="2.0.2"
+VERSION="2.0.3"
 AC_PROG_INSTALL
 AC_PATH_PROG(PERL, perl, no)
 AC_PATH_PROG(SED, sed, no)

share/config/templates.php.in
@@ -60,6 +60,7 @@ define('TEMPLATE_LOGIN', TEMPLATE_DIR . 'login.html');
 
 define('TEMPLATE_ADD_EDIT', TEMPLATE_DIR . 'add-edit_notification.html');
 define('TEMPLATE_ADD_EDIT_CONTACTS_METHODS', TEMPLATE_DIR . 'add-edit_notification_contacts_methods.html');
+define('TEMPLATE_ADD_EDIT_TYPES', TEMPLATE_DIR . 'add-edit_notification_contacts_types.html');
 define('TEMPLATE_ADD_EDIT_LET_NOTIFIER_HANDLE', TEMPLATE_DIR . 'add-edit_let_notifier_handle.html');
 
 define('TEMPLATE_CONTACT_MANAGER', TEMPLATE_DIR . 'contact_manager.html');

share/inc/content_add-edit.php
@@ -212,7 +212,7 @@ function getContent () {
 	if (!empty($id)) {
 		// get escalations
 		$query = sprintf(
-			'select id,on_ok,on_warning,on_critical,on_unknown,on_host_up,on_host_unreachable,on_host_down,on_type_problem,on_type_recovery,on_type_flappingstart,on_type_flappingstop,on_type_flappingdisabled,on_type_downtimestart,on_type_downtimeend,on_type_downtimecancelled,on_type_acknowledgement,on_type_custom,notify_after_tries
+			'select id,notify_after_tries
 				from escalations_contacts
 				where notification_id=\'%s\'
 				order by notify_after_tries asc',
@@ -247,7 +247,36 @@ function getContent () {
 		// assign 'let notifier handle'
 		if (!$x) {
 			$templateSubSubContent = new nwTemplate(TEMPLATE_ADD_EDIT_LET_NOTIFIER_HANDLE);
+		    $templateSubContentTypes = new nwTemplate(TEMPLATE_ADD_EDIT_TYPES);
+			$templateSubContentTypes->assign('NOTIFY_PROBLEM', NOTIFY_PROBLEM);
+			$templateSubContentTypes->assign('NOTIFY_RECOVERY', NOTIFY_RECOVERY);
+			$templateSubContentTypes->assign('NOTIFY_SPECIAL', NOTIFY_SPECIAL);
+			$templateSubContentTypes->assign('NOTIFY_OK', NOTIFY_OK);
+			$templateSubContentTypes->assign('NOTIFY_WARNING', NOTIFY_WARNING);
+			$templateSubContentTypes->assign('NOTIFY_CRITICAL', NOTIFY_CRITICAL);
+			$templateSubContentTypes->assign('NOTIFY_UNKNOWN', NOTIFY_UNKNOWN);
+			$templateSubContentTypes->assign('NOTIFY_UP', NOTIFY_UP);
+			$templateSubContentTypes->assign('NOTIFY_UNREACHABLE', NOTIFY_UNREACHABLE);
+			$templateSubContentTypes->assign('NOTIFY_DOWN', NOTIFY_DOWN);
+			$templateSubContentTypes->assign('NOTIFY_FLAPPING_START', NOTIFY_FLAPPING_START);
+			$templateSubContentTypes->assign('NOTIFY_FLAPPING_STOP', NOTIFY_FLAPPING_STOP);
+			$templateSubContentTypes->assign('NOTIFY_FLAPPING_DISABLED', NOTIFY_FLAPPING_DISABLED);
+			$templateSubContentTypes->assign('NOTIFY_DOWNTIME_START', NOTIFY_DOWNTIME_START);
+			$templateSubContentTypes->assign('NOTIFY_DOWNTIME_END', NOTIFY_DOWNTIME_END);
+			$templateSubContentTypes->assign('NOTIFY_DOWNTIME_CANCELLED', NOTIFY_DOWNTIME_CANCELLED);
+			$templateSubContentTypes->assign('NOTIFY_ACKNOWLEDGEMENT', NOTIFY_ACKNOWLEDGEMENT);
+			$templateSubContentTypes->assign('NOTIFY_CUSTOM', NOTIFY_CUSTOM);
+
+			$templateSubContentTypes->assign('NOTIFY_ADVANCED', NOTIFY_ADVANCED);
+			$templateSubContentTypes->assign('NOTIFY_WARNING_TO_CRITICAL', NOTIFY_WARNING_TO_CRITICAL);
+			$templateSubContentTypes->assign('NOTIFY_WARNING_TO_UNKNOWN', NOTIFY_WARNING_TO_UNKNOWN);
+			$templateSubContentTypes->assign('NOTIFY_CRITICAL_TO_WARNING', NOTIFY_CRITICAL_TO_WARNING);
+			$templateSubContentTypes->assign('NOTIFY_CRITICAL_TO_UNKNOWN', NOTIFY_CRITICAL_TO_UNKNOWN);
+			$templateSubContentTypes->assign('NOTIFY_UNKNOWN_TO_CRITICAL', NOTIFY_UNKNOWN_TO_CRITICAL);
+			$templateSubContentTypes->assign('NOTIFY_UNKNOWN_TO_WARNING', NOTIFY_UNKNOWN_TO_WARNING);
+
 			$templateSubSubContent->assign('ADD_EDIT_LET_NOTIFIER_HANDLE', ADD_EDIT_LET_NOTIFIER_HANDLE);
+			$templateSubContentTypes->assign('ADD_EDIT_NOTIFY_ON_TABLE', ADD_EDIT_NOTIFY_ON_TABLE);
 			// $templateSubSubContent->assign('ADD_EDIT_RELOOP_DELAY', ADD_EDIT_RELOOP_DELAY);
 			$templateSubSubContent->assign('ADD_EDIT_ROLLOVER', ADD_EDIT_ROLLOVER);
 			$templateSubSubContent->assign('CHECKED_LET_NOTIFIER_HANDLE', ($dbResult[0]['let_notifier_handle']==1)?' checked="checked" ':'');
@@ -354,31 +383,49 @@ function getContent () {
 		$templateSubContent->assign('ADD_EDIT_NOTIFY_USERS', ADD_EDIT_NOTIFY_USERS);
 		$templateSubContent->assign('ADD_EDIT_NOTIFY_GROUPS', ADD_EDIT_NOTIFY_GROUPS);
 		$templateSubContent->assign('ADD_EDIT_NOTIFY_BY', ADD_EDIT_NOTIFY_BY);
-		$templateSubContent->assign('ADD_EDIT_NOTIFY_ON', ADD_EDIT_NOTIFY_ON);
+        if (!$x) {
+            $templateSubContent->assign('ADD_EDIT_NOTIFY_ON', ADD_EDIT_NOTIFY_ON);
+        }
 
                 $templateSubContent->assign('NOTIFY_USERS_SELECT', htmlSelect('notify_users' . $formArray . '[]', getContacts(), $notify_users, 'size="5" multiple="multiple"'));
 		$templateSubContent->assign('NOTIFY_GROUPS_SELECT', htmlSelect('notify_groups' . $formArray . '[]', getContactGroups(), $notify_groups, 'size="5" multiple="multiple"'));
 
 		$templateSubContent->assign('NOTIFY_BY_SELECT', htmlSelect('notify_by' . $formArray . '[]', getNotificationMethods(), $notify_by, 'size="5" multiple="multiple"'));
 
-		$templateSubContent->assign('CHECKED_NOTIFY_OK', ($dbResult[$x]['on_ok']=='1') ? ' checked="checked"' : '' );
-		$templateSubContent->assign('CHECKED_NOTIFY_WARNING', ($dbResult[$x]['on_warning']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_UNKNOWN', ($dbResult[$x]['on_unknown']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_HOST_UNREACHABLE', ($dbResult[$x]['on_host_unreachable']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_CRITICAL', ($dbResult[$x]['on_critical']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_HOST_UP', ($dbResult[$x]['on_host_up']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_HOST_DOWN', ($dbResult[$x]['on_host_down']=='1') ? ' checked="checked"' : '' );
+        if (!$x) {
+            // only show the check boxes for the base rule, not notifications
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_OK', ($dbResult[$x]['on_ok']==1) ? ' checked="checked"' : '' );
 
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_PROBLEM', ($dbResult[$x]['on_type_problem']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_RECOVERY', ($dbResult[$x]['on_type_recovery']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_FLAPPINGSTART', ($dbResult[$x]['on_type_flappingstart']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_FLAPPINGSTOP', ($dbResult[$x]['on_type_flappingstop']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_FLAPPINGDISABLED', ($dbResult[$x]['on_type_flappingdisabled']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_DOWNTIMESTART', ($dbResult[$x]['on_type_downtimestart']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_DOWNTIMEEND', ($dbResult[$x]['on_type_downtimeend']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_DOWNTIMECANCELLED', ($dbResult[$x]['on_type_downtimecancelled']=='1') ? ' checked="checked"' : '' );
-                $templateSubContent->assign('CHECKED_NOTIFY_TYPE_ACKNOWLEDGEMENT', ($dbResult[$x]['on_type_acknowledgement']=='1') ? ' checked="checked"' : '' );
-		$templateSubContent->assign('CHECKED_NOTIFY_TYPE_CUSTOM', ($dbResult[$x]['on_type_custom']==1)?' checked="checked" ':'');
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_WARNING', (($dbResult[$x]['on_warning'] & 1) == 1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_CRITICAL_TO_WARNING', (($dbResult[$x]['on_warning'] & 8) == 8) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_UNKNOWN_TO_WARNING', (($dbResult[$x]['on_warning'] & 16) == 16) ? ' checked="checked"' : '' );
+
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_CRITICAL', (($dbResult[$x]['on_critical'] & 1) == 1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_WARNING_TO_CRITICAL', (($dbResult[$x]['on_critical'] & 4) == 4) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_UNKNOWN_TO_CRITICAL', (($dbResult[$x]['on_critical'] & 16) == 16) ? ' checked="checked"' : '' );
+
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_UNKNOWN', (($dbResult[$x]['on_unknown'] & 1) == 1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_WARNING_TO_UNKNOWN', (($dbResult[$x]['on_unknown'] & 4) == 4) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_CRITICAL_TO_UNKNOWN', (($dbResult[$x]['on_unknown'] & 8) == 8) ? ' checked="checked"' : '' );
+
+
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_HOST_UP', ($dbResult[$x]['on_host_up']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_HOST_DOWN', ($dbResult[$x]['on_host_down']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_HOST_UNREACHABLE', ($dbResult[$x]['on_host_unreachable']==1) ? ' checked="checked"' : '' );
+
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_PROBLEM', ($dbResult[$x]['on_type_problem']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_RECOVERY', ($dbResult[$x]['on_type_recovery']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_FLAPPINGSTART', ($dbResult[$x]['on_type_flappingstart']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_FLAPPINGSTOP', ($dbResult[$x]['on_type_flappingstop']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_FLAPPINGDISABLED', ($dbResult[$x]['on_type_flappingdisabled']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_DOWNTIMESTART', ($dbResult[$x]['on_type_downtimestart']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_DOWNTIMEEND', ($dbResult[$x]['on_type_downtimeend']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_DOWNTIMECANCELLED', ($dbResult[$x]['on_type_downtimecancelled']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_ACKNOWLEDGEMENT', ($dbResult[$x]['on_type_acknowledgement']==1) ? ' checked="checked"' : '' );
+            $templateSubContentTypes->assign('CHECKED_NOTIFY_TYPE_CUSTOM', ($dbResult[$x]['on_type_custom']==1)?' checked="checked" ':'');
+            $templateSubContentTypes->assign('ARRAY_ITEM', $formArray);
+            $templateSubContent->assign('NOTIFY_ON_TABLE', $templateSubContentTypes->getHTML());
+        }
 
 		$templateSubContent->assign('ARRAY_ITEM', $formArray);
 

share/inc/noma-backend-sqlite3.php
@@ -77,7 +77,7 @@ function querySQLite3DB ($query, $return_count = false, $ndo = false) {
 	
 	$result = $db->query($query);
     if ($result === FALSE) {
-        die(print_r($db->errorInfo()).'<br><br>Query: '.$query.'<br>');
+        die(print_r($db->errorInfo()).'<br><br>Query: '.$query.'<br>'.print_r(debug_backtrace()));
     }
 
 

share/lang/de.php
@@ -83,7 +83,7 @@ define('OVERVIEW_HEADING_SERVICES', 'Services');
 define('OVERVIEW_HEADING_OWNER', 'Besitzer');
 define('OVERVIEW_HEADING_NOTIFICATION_RULE', 'Nr.');
 define('OVERVIEW_HEADING_TIMEZONE', 'Zeitzone');
-define('OVERVIEW_HEADING_NOTIFY_ON', 'Benachr. bei');
+define('OVERVIEW_HEADING_NOTIFY_ON', 'Benachr. Filter');
 define('OVERVIEW_HEADING_NOTIFY_BY', 'Benachr. durch');
 define('OVERVIEW_HEADING_NOTIFY_USERS', 'Zu benachr. Personen');
 define('OVERVIEW_LONG_INCLUDE_HEADING', 'Eingeschlossen');
@@ -130,8 +130,8 @@ define('ADD_EDIT_TIMEFRAME', 'Zeitplan:');
 define('ADD_EDIT_OWNER', 'Besitzer:');
 define('ADD_EDIT_NOTIFY_USERS', 'Zu benachrichtigende Personen:');
 define('ADD_EDIT_NOTIFY_GROUPS', 'Zu benachrichtigende Gruppen:');
-define('ADD_EDIT_NOTIFY_BY', 'Nachrich per:');
-define('ADD_EDIT_NOTIFY_ON', 'Nachricht bei:');
+define('ADD_EDIT_NOTIFY_BY', 'Nachricht per:');
+define('ADD_EDIT_NOTIFY_ON', 'Nachricht Filter:');
 define('ADD_EDIT_SUBMIT', 'Speichern');
 define('ADD_EDIT_BUTTON_ADD_ESCALATION', 'Eskalation hinzuf&uuml;gen');
 define('ADD_EDIT_CONFIRM_ADD_ESCALATION', 'Alle ungesicherten Daten gehen verloren! Sicher?');
@@ -141,7 +141,7 @@ define('ADD_EDIT_NOTIFY_AFTER_TRIES', 'Benachrichtigung nach:');
 define('ADD_EDIT_LET_NOTIFIER_HANDLE', 'Eskalationsvorgehen an Notifier &uuml;bergeben:');
 define('ADD_EDIT_RELOOP_DELAY', 'Verz&ouml;gerung (in s) zwischen Eskalationen:');
 define('ADD_EDIT_ROLLOVER', 'Z&auml;hler zur&uuml;cksetzen bei &Uuml;berlauf:');
-define('ADD_EDIT_NUM_NOTIFICATIONS', '(Anzahl)');
+define('ADD_EDIT_NUM_NOTIFICATIONS', 'Anzahl (alle Arten)');
 define('ADD_EDIT_ESCALATION_DELETED', 'Eskalation wurde gel&ouml;scht.');
 define('ADD_EDIT_ESCALATION_ERROR', 'Fehler w&auml;hrend der Operation!');
 define('ADD_EDIT_NOTIFICATION_ADD_UPDATE_ERROR', 'Fehler w&auml;hrend der Operation!');
@@ -354,5 +354,30 @@ define('CONTACTGROUPS_TITLE_NOTIFICATION_ACTIVE', 'Aktive Regel');
 define('CONTACTGROUPS_TITLE_NOTIFICATION_NOTIFY_AFTER_TRIES', 'Nach x Versuchen benachrichtigen');
 define('CONTACTGROUPS_TITLE_ESCALATION_NOTIFY_AFTER_TRIES', 'Nach x Versuchen benachrichtigen');
 define('CONTACTGROUPS_TITLE_TIMEFRAME_NAME', 'Name des Zeitplans');
+define('NOTIFY_PROBLEM', 'Problem');
+define('NOTIFY_RECOVERY', 'Erholung');
+define('NOTIFY_SPECIAL', 'Andere');
+define('NOTIFY_OK', 'OK');
+define('NOTIFY_WARNING', 'Warnung');
+define('NOTIFY_CRITICAL', 'Kritischer Zustand');
+define('NOTIFY_UNKNOWN', 'Unbekannt');
+define('NOTIFY_UP', 'Host an');
+define('NOTIFY_UNREACHABLE', 'Host nicht erreichbar');
+define('NOTIFY_DOWN', 'Host aus');
+define('NOTIFY_FLAPPING_START', 'Flattern angefangen');
+define('NOTIFY_FLAPPING_STOP', 'Flattern beendet');
+define('NOTIFY_FLAPPING_DISABLED', 'Flatter-Erkennung ausgeschaltet');
+define('NOTIFY_DOWNTIME_START', 'Geplante Ausfallzeit angefangen');
+define('NOTIFY_DOWNTIME_END', 'Geplante Ausfallzeit beendet');
+define('NOTIFY_DOWNTIME_CANCELLED', 'Geplante Ausfallzeit abgebrochen');
+define('NOTIFY_ACKNOWLEDGEMENT', 'Best&auml;tigung');
+define('NOTIFY_CUSTOM', 'Benutzerdefiniert');
+define('NOTIFY_ADVANCED', 'Umwandlungen des Problems'
+define('NOTIFY_WARNING_TO_CRITICAL', 'Warnung auf kritisch');
+define('NOTIFY_WARNING_TO_UNKNOWN', 'Warnung auf Unbekannt');
+define('NOTIFY_CRITICAL_TO_WARNING', 'Kritisch auf Warnung');
+define('NOTIFY_CRITICAL_TO_UNKNOWN', 'Kritisch auf Unbekannt');
+define('NOTIFY_UNKNOWN_TO_CRITICAL', 'Unbekannt auf kritisch');
+define('NOTIFY_UNKNOWN_TO_WARNING', 'Unbekannt auf Warnung');
 
 ?>

share/lang/en.php
@@ -83,7 +83,7 @@ define('OVERVIEW_HEADING_SERVICES', 'Services');
 define('OVERVIEW_HEADING_OWNER', 'Owner');
 define('OVERVIEW_HEADING_NOTIFICATION_RULE', 'No.');
 define('OVERVIEW_HEADING_TIMEZONE', 'Timezone');
-define('OVERVIEW_HEADING_NOTIFY_ON', 'Notify on');
+define('OVERVIEW_HEADING_NOTIFY_ON', 'Filter on');
 define('OVERVIEW_HEADING_NOTIFY_BY', 'Notify by');
 define('OVERVIEW_HEADING_NOTIFY_USERS', 'Notify users');
 define('OVERVIEW_LONG_INCLUDE_HEADING', 'Include');
@@ -131,7 +131,7 @@ define('ADD_EDIT_OWNER', 'Owner:');
 define('ADD_EDIT_NOTIFY_USERS', 'Notify users:');
 define('ADD_EDIT_NOTIFY_GROUPS', 'Notify groups:');
 define('ADD_EDIT_NOTIFY_BY', 'Notify by:');
-define('ADD_EDIT_NOTIFY_ON', 'Notify on:');
+define('ADD_EDIT_NOTIFY_ON', 'Filter on:');
 define('ADD_EDIT_SUBMIT', 'Save');
 define('ADD_EDIT_BUTTON_ADD_ESCALATION', 'Add escalation');
 define('ADD_EDIT_CONFIRM_ADD_ESCALATION', 'All unsaved data will be lost. Are you sure?');
@@ -141,7 +141,7 @@ define('ADD_EDIT_NOTIFY_AFTER_TRIES', 'Notify after:');
 define('ADD_EDIT_LET_NOTIFIER_HANDLE', 'Let notifier handle escalations:');
 define('ADD_EDIT_RELOOP_DELAY', 'Delay (in s) between escalations:');
 define('ADD_EDIT_ROLLOVER', 'Rollover if the last rule is reached:');
-define('ADD_EDIT_NUM_NOTIFICATIONS', '(number of notifications)');
+define('ADD_EDIT_NUM_NOTIFICATIONS', 'number of notifications (all types)');
 define('ADD_EDIT_ESCALATION_DELETED', 'Escalation has been deleted.');
 define('ADD_EDIT_ESCALATION_ERROR', 'An error occurred!');
 define('ADD_EDIT_NOTIFICATION_ADD_UPDATE_ERROR', 'An error occurred!');
@@ -354,5 +354,31 @@ define('CONTACTGROUPS_TITLE_NOTIFICATION_ACTIVE', 'Active rule');
 define('CONTACTGROUPS_TITLE_NOTIFICATION_NOTIFY_AFTER_TRIES', 'Notify after tries');
 define('CONTACTGROUPS_TITLE_ESCALATION_NOTIFY_AFTER_TRIES', 'Notify after tries');
 define('CONTACTGROUPS_TITLE_TIMEFRAME_NAME', 'Timeframe name');
+define('NOTIFY_PROBLEM', 'Problem');
+define('NOTIFY_RECOVERY', 'Recovery');
+define('NOTIFY_SPECIAL', 'Other');
+define('NOTIFY_OK', 'OK');
+define('NOTIFY_WARNING', 'Warning');
+define('NOTIFY_CRITICAL', 'Critical');
+define('NOTIFY_UNKNOWN', 'Unknown');
+define('NOTIFY_UP', 'Host Up');
+define('NOTIFY_UNREACHABLE', 'Host Unreachable');
+define('NOTIFY_DOWN', 'Host Down');
+define('NOTIFY_FLAPPING_START', 'Flapping started');
+define('NOTIFY_FLAPPING_STOP', 'Flapping stopped');
+define('NOTIFY_FLAPPING_DISABLED', 'Flap detection disabled');
+define('NOTIFY_DOWNTIME_START', 'Downtime started');
+define('NOTIFY_DOWNTIME_END', 'Downtime ended');
+define('NOTIFY_DOWNTIME_CANCELLED', 'Downtime cancelled');
+define('NOTIFY_ACKNOWLEDGEMENT', 'Acknowledgement');
+define('NOTIFY_CUSTOM', 'Custom');
+define('NOTIFY_ADVANCED', 'Problem transitions');
+define('NOTIFY_WARNING_TO_CRITICAL', 'Warning to critical');
+define('NOTIFY_WARNING_TO_UNKNOWN', 'Warning to unknown');
+define('NOTIFY_CRITICAL_TO_WARNING', 'Critical to warning');
+define('NOTIFY_CRITICAL_TO_UNKNOWN', 'Critical to unknown');
+define('NOTIFY_UNKNOWN_TO_CRITICAL', 'Unknown to critical');
+define('NOTIFY_UNKNOWN_TO_WARNING', 'Unknown to warning');
+
 
 ?>

commit a498f7970e43ee74b03d62817142bbb48642bcf9

* Fixed bug introduced by change 988aa5d36de7dd679ce2233b7c63b54bef19803e affecting new rules
* The getMaxValue() function was broken -> fixed it and added a test

notifier/lib/contacts.pm
@@ -619,6 +619,9 @@ sub getMaxValue
     # e.g. 1-4,7-8,12
 	my ($range) = @_;
 
+    my $min;
+    my $max;
+
 	$range =~ s/[^0-9,;-]//g;
 
     return $range unless ($range =~ /[,;-]+/);
@@ -626,22 +629,28 @@ sub getMaxValue
     my $newmax = 1;
     foreach my $crange (split(/[,;]/, $range))
     {
-        debug("Expanding $crange", 2);
-        $crange =~ /(\d*)-(\d*)/;
+		if ($crange =~ /-/)
+		{
+			debug("Expanding $crange", 3);
+			$crange =~ /(\d*)-(\d*)/;
 
-        my $min = $1;
-        my $max = $2;
+			$min = $1;
+			$max = $2;
 
-        if ((not defined($min)) or ($min < 1))
-        {
-            debug("Invalid minimum value in range \"$crange\" - setting to 1", 3);
-            $min = 1;
-        }
+			if ((not defined($min)) or ($min < 1))
+			{
+				debug("Invalid minimum value in range \"$crange\" - setting to 1", 1);
+				$min = 1;
+			}
 
-        if ((not defined($max)) or ($max < $min))
-        {
-            debug("Invalid maximum value in range \"$crange\" - setting to 99999", 3);
-            $max = 99999;
+			if ((not defined($max)) or ($max < $min))
+			{
+				debug("Invalid maximum value in range \"$crange\" - setting to 99999", 1);
+				$max = 99999;
+			}
+		} else {
+            debug("Testing $crange", 3);
+            $max = $crange;
         }
 
 

notifier/noma_daemon.pl.in
@@ -399,14 +399,14 @@ do
             'SELECT id,recipients_include,recipients_exclude,hosts_include,hosts_exclude,hostgroups_include,hostgroups_exclude,services_include,services_exclude,servicegroups_include,servicegroups_exclude FROM notifications';
             if ( $cmdh{check_type} eq 'h' )
             {
-                $query .= ' where ' . $stati_host{$cmdh{status}} . '=\'1\' and ' . $stati_type{$cmdh{notification_type}} . '=\'1\'';
+                $query .= ' where ' . $stati_host{$cmdh{status}} . '=\'1\'';
             } elsif ( $cmdh{notification_type} eq 'PROBLEM' || $cmdh{notification_type} eq 'RECOVERY')
             {
                 $query .= ' where (' . $stati_service{$cmdh{status}} . ' & 1) = 1';
                 $query .= ' OR (' . $stati_service{$cmdh{status}} . ' & ' .$stati_previous{$last_state} .') = ' .$stati_previous{$last_state};
             } else
             {
-                $query .= ' where (' . $stati_service{$cmdh{status}} . ' & 1) = 1 and ' . $stati_type{$cmdh{notification_type}} . '=\'1\'';
+                $query .= ' where (' . $stati_service{$cmdh{status}} . ' & 1) = 1';
             }
 
             # only active rules!

t/maxValue.t
@@ -1,0 +1,43 @@
+#!/usr/bin/perl
+
+use Test::More;
+use FindBin qw($Bin);
+use lib "$Bin/../notifier/lib";
+
+
+
+use contacts;
+
+
+sub debug
+{
+	my ($str, $val) = (@_);
+	print "$str\n";
+}
+
+
+my @output;
+
+my @testdata = (
+
+    {
+        'input'   => '1-4,7-8,12',
+        'solloutput' => [ '12' ]
+    },
+    {
+        'input'   => '1,2',
+        'solloutput' => [ '2' ]
+    }
+);
+
+plan tests => ($#testdata + 1);
+
+foreach my $test (@testdata)
+{
+	my @solloutput = @{$test->{solloutput}};
+	@output = getMaxValue($test->{input});
+
+	# @output = sort @output;
+
+	is_deeply (\@output, \@solloutput, 'For range '.$test->{input}.' expected:'.join(',', @solloutput));
+}